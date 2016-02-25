3e94a7d0cff17e1110d23f9b1ecfe3fa3ca083e5
lovekidan <lovekidan@qq.com>
2016-02-25 23:50:38 +0800

    first time to build image_compress success!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include "pngimage/png_image.hpp"
 
 int main(int argc, char** argv) {
-	std::string image_path = "/XX";
+	//string image_path;
 	printf("welcome to use image compress.\n");
 	PngImage* png_image_ = new PngImage();
-	png_image_->read_png(image_path);
+	printf("argc=%d\n", argc);
+	if (2 == argc)
+	{
+		png_image_->read_png(argv[1]);
+		printf("%s 's type is %d\n", argv[1], png_image_->getPngType());
+	}
 	return 0;
 }
